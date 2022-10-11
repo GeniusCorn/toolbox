01b200ec84eda57b09ecbd470ad7ee0e5fd47db1
GeniusCorn <hi@nicecorn.com>
2022-10-11 21:02:17 +0800

    feat: 404 page

---
+++ src/layouts/404.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import router from '@/router'
+</script>
+
+<template>
+  <NavBar />
+
+  <div flex="~ col" gap-4 p-6 justify-center items-center>
+    <div>404 not found</div>
+    <button @click="router.push('/')">返回</button>
+  </div>
+
+  <FooterBottom />
+</template>
+
+<style scoped></style>
